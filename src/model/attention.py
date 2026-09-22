4047dc02b5f1a718c8efdd89a5a90a86989935b3
HalasTomasz <halastomasz00@gmail.com>
2026-09-22 14:43:30 +0200

    Adding absorbed func

--- src/model/attention.py
+++ src/model/attention.py
@@ -1,5 +1,6 @@
 import math
 
+import torch
 from torch import nn
 
 from src.model.model_args import DeepSeekV3ModelArgs
@@ -51,8 +52,132 @@ class Attention(nn.Module):
         # Implement the attention mechanism here
         pass
 
-    def init_weights(self, init_std: float) -> None:
+    def init_weights(
+        self,
+        init_std: float,
+        buffer_device: torch.device | None = None,
+    ):
         """Initialize linear projections used by this attention layer."""
-        for module in self.modules():
-            if isinstance(module, nn.Linear):
-                nn.init.normal_(module.weight, mean=0.0, std=init_std)
+        linear_list = [
+            self.wkv_a,
+            self.wkv_b,
+        ]
+        # if we scale Q with LoRA
+        if self.q_lora_rank > 0:
+            linear_list.extend([self.wq_a, self.wq_b])
+        else:
+            linear_list.append(self.wq)
+
+        for linear in linear_list:
+            nn.init.normal_(linear.weight, mean=0.0, std=0.02)
+        nn.init.trunc_normal_(self.wo.weight, mean=0.0, std=init_std)
+
+        self.kv_norm.reset_parameters()
+        if self.q_lora_rank > 0:
+            self.q_norm.reset_parameters()
+
+    @torch.no_grad()
+    def absorb_mla_weights(self):
+        """Absorb the weights of the LoRA layers into the main Q/K/V linear layers."""
+
+        if self.q_lora_rank != 0:
+            raise NotImplementedError("Absorbing LoRA weights for Q is not implemented yet.")
+
+        n_heads = self.n_heads
+        dim = self.dim
+        qk_rope_head_dim = self.qk_rope_head_dim
+        qk_nope_head_dim = self.qk_nope_head_dim
+        v_head_dim = self.v_head_dim
+        kv_lora_rank = self.kv_lora_rank
+
+        device = self.wq.weight.device
+        dtype = self.wq.weight.dtype
+
+        wq = self.wq.weight.view(
+            n_heads,
+            qk_nope_head_dim + qk_rope_head_dim,
+            dim,
+        )
+
+        # qk_nope: [n_heads, qk_nope_head_dim, dim]
+        # qk_rope: [n_heads, qk_rope_head_dim, dim]
+
+        wq_nope, wq_rope = torch.split(
+            wq,
+            [qk_nope_head_dim, qk_rope_head_dim],
+            dim=1,
+        )
+
+        wkv_b = self.wkv_b.weight.view(
+            n_heads,
+            qk_nope_head_dim + v_head_dim,
+            kv_lora_rank,
+        )
+
+        # w_uk: [n_heads, qk_nope_head_dim, kv_lora_rank]
+        # w_uv: [n_heads, v_head_dim, kv_lora_rank]
+        w_uk, w_uv = torch.split(
+            wkv_b,
+            [qk_nope_head_dim, v_head_dim],
+            dim=1,
+        )
+
+        # [n_heads, kv_lora_rank, dim]
+        wq_absorbed_nope = torch.bmm(
+            w_uk.float().transpose(
+                1, 2
+            ),  # [n_heads, qk_nope_head_dim, kv_lora_rank] -> [n_heads, kv_lora_rank, qk_nope_head_dim]
+            wq_nope.float(),  # [n_heads, qk_nope_head_dim, dim]
+        ).to(dtype=dtype)
+
+        # Each new query head is [absorbed nope | original RoPE].
+        wq_absorbed = torch.cat(
+            [wq_absorbed_nope, wq_rope],
+            dim=1,
+        ).reshape(
+            n_heads * (kv_lora_rank + qk_rope_head_dim),
+            dim,
+        )
+
+        self.wq_absorbed = nn.Linear(
+            dim,
+            n_heads * (kv_lora_rank + qk_rope_head_dim),
+            bias=False,
+            device=device,
+            dtype=dtype,
+        )
+        self.wq_absorbed.weight.copy_(wq_absorbed)
+        self.wq_absorbed.requires_grad_(False)  # Freeze the absorbed weights
+
+        # [dim, n_heads, v_head_dim] -> [n_heads, dim, v_head_dim]
+        w_o = self.wo.weight.view(
+            dim,
+            n_heads,
+            v_head_dim,
+        ).permute(1, 0, 2)
+
+        # [n_heads, dim, v_head_dim] @ [n_heads, v_head_dim, kv_lora_rank] -> [n_heads, dim, kv_lora_rank]
+        w_o_absorbed_per_head = torch.bmm(
+            w_o.float(),
+            w_uv.float(),
+        ).to(dtype=dtype)
+
+        # [n_heads, dim, kv_lora_rank] -> [dim, n_heads, kv_lora_rank] -> [dim, n_heads * kv_lora_rank]
+        w_o_absorbed = w_o_absorbed_per_head.permute(
+            1,
+            0,
+            2,
+        ).reshape(
+            dim,
+            n_heads * kv_lora_rank,
+        )
+
+        self.wo_absorbed = nn.Linear(
+            n_heads * kv_lora_rank,
+            dim,
+            bias=False,
+            device=device,
+            dtype=dtype,
+        )
+        self.wo_absorbed.weight.copy_(w_o_absorbed)
+        self.wo_absorbed.requires_grad_(False)
